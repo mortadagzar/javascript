404c3189c4d49635814ec35bae5f70c7a3ca4e49
mortada gzar <mortadagzar@gmail.com>
2018-11-04 14:02:36 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-print("{:>20}".format("text") )
-print("{:>21}".format("text") )
-print("{:>22}".format("text") )
-print("{:>23}".format("text") )
-print("{:>24}".format("text") )
-print("{:>25}".format("text") )
-print("{:>26}".format("text") )
-print("{:>25}".format("text") )
-print("{:>24}".format("text") )
-print("{:>23}".format("text") )
-print("{:>22}".format("text") )
-print("{:>21}".format("text") )
-print("{:>20}".format("text") )
+print('hello')
+
+
