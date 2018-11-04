b66de0f94814859ccc859664b0e8bc581c0b4dea
mortada gzar <mortadagzar@gmail.com>
2018-11-03 23:13:32 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,13 @@
-class Circle {
-    constructor(dimeter1,dimeter2,dimeter3){
-       
-        this.dimeter1=56;
-        this.dimeter2=33;
-        this.dimeter3=99;
-        console.log("my name is Fahad" + dimeter1 +" "+ dimeter2+" "+ dimeter3);
-    }
+
+walk();
+
+function walk (){
+console.log("walk is fire");
 }
 
-let myCircle=new Circle();
+
+const run=function(){
+    console.log("run is fire");
+}
+
+run();
