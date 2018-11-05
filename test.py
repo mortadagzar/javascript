2811368ac8d180236c73c44afbe41a8e6365eb2e
mortada gzar <mortadagzar@gmail.com>
2018-11-04 22:15:06 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 
 for i in range(1,7):
     print('{:<3}|'.format("your name is "),end="    ")
-    for i in range(1,30):
-        print('{:<3}|'.format("LOL"),end="    ")
-for i in range(1,7):
-    print('{:<2}|'.format("your name is "),end="    ")
-    for i in range(1,30):
-        print('{:<2}|'.format("OLO"),end="    ")        
+    for
