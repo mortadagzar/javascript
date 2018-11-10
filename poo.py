992c2ae9b30e8462005f24f2ad7d6ea1d2193071
mortada gzar <mortadagzar@gmail.com>
2018-11-09 20:04:12 -0800

    fy

--- poo.py
+++ poo.py
@@ -1,11 +1,2 @@
-
-root=input('input R value = ')
-
-
-if root > 10 :
-    print('The value of X is :True, R is less than 10, it equal to:' ,root)
-else:
-    print('The value of X is :False,R is greater than 10, it is equal to:',root)  
-
-
-
+import sqlite3
+con=sqlite3.connect('best.db')
